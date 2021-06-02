e03d62c8807ca869a4afee3d6e9c854190cf4b9e
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2021-06-03 00:54:32 +0200

    Updates
    Directories simplified and picker fixed

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,15 +1,25 @@
 import React, {useState} from 'react';
-import { ImageBackground, StyleSheet, View, Text, TextInput} from 'react-native';
+import { ImageBackground, StyleSheet, View} from 'react-native';
+import * as Yup from 'yup';
+
+import { AppForm, AppFormField, AppFormPicker, SubmitButton } from "../components/forms";
+
 import AppButton from '../components/AppButton';
-import AppPicker from '../components/AppPicker';
+
+//NOTE : not sure how to include the picker in Formik and for validation in Yup
+//NOTE : need to make the register button the full width and height of the bottom portion of screen
 
 const user_types = [
     { label : "Driver", value : 1},
     { label : "Customer", value : 2},
 ];
 
-const [user_type, set_user_type] = useState();
-
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password"),
+    user: Yup.object().required().nullable().label("User Type")
+  });
+  
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
@@ -17,24 +27,36 @@ function WelcomeScreen(props) {
             source = {require('../assets/Messenger-pana.png')}
         >
             <View style = {styles.loginContainer}>
-                <AppPicker 
-                    selectedItem = {user_type}
-                    onSelectItem = {item => set_user_type(item)}
-                    items = {user_types}
-                    placeholder = "Type of user" ></AppPicker>
-                <AppTextInput 
-                    value = {text}
-                    placeholder = "Username/Email"
-                    textContentType = {username}
-                />
-                <AppTextInput 
-                    value = {text}
-                    placeholder = "Password"
-                    textContentType = {password}
-                    secureTextEntry = {true}
-                />
-                <AppButton title = "Sign In" />
+                <AppForm
+                    initialValues={{ user: null, email: "", password: "" }}
+                    onSubmit={(values) => console.log(values)}
+                    validationSchema={validationSchema}
+                >
+                  <AppFormPicker 
+                        items = {user_types}
+                        name = "user"
+                        placeholder = "Driver/Customer" 
+                        />
+                        <AppFormField
+                        autoCapitalize = "none"
+                        autoCorrect = {false}
+                        keyboardType = "email-address"
+                        name = "email"
+                        placeholder = "Email"
+                        textContentType = "emailAddress"
+                        />
+                        <AppFormField
+                        autoCapitalize = "none"
+                        autoCorrect = {false}
+                        name = "password"
+                        placeholder = "Password"
+                        textContentType = "password"
+                        secureTextEntry
+                        />
+                        <SubmitButton title = "Sign In"/>     
+                </AppForm>
             </View>
+            <AppButton title = "Don't have an account yet?" onPress = {() => console.log("Create new account")}/>    
         </ImageBackground>
     );
 }
@@ -47,7 +69,7 @@ const styles = StyleSheet.create({
     },
     loginContainer: {
         padding : 20,
-        width : "100%",
+        width : "80%",
     },
 
 });

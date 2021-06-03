6059a1a419003c92fb2f91b3f9363956fb911152
Ihlaam <47963182+Ihlaam@users.noreply.github.com>
2021-06-04 01:30:20 +0200

    New order screen done

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import CustQuoteScreen from "./app/screens/CustQuoteScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />  
-    </NavigationContainer>
-  );
+  return <CustQuoteScreen />
+    // <NavigationContainer theme={navigationTheme}>
+    //   <AppNavigator />  
+    // </NavigationContainer>
 }
 
 //StartNavigator is for welcome/login/register screens

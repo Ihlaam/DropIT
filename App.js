445af28b9b8ed5fa57a7c0b5889d8ee940df88d4
Ironmien <armien.ab@gmail.com>
2021-06-12 11:22:58 +0200

    request api order

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import CustCreateOrderScreen from "./app/screens/CustCreateOrderScreen";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
+  
   return(
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />  

3c908b771b09433521c4246734ee580debd135f5
Juuuub <admin@delivast.co.za>
2021-06-19 17:22:34 +0200

    Remove Dynamic IP setting

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,8 @@
 import {create} from 'apisauce';
-const ip = require('ip');
 
 const apiClient = create
  ({
-    baseURL:`${ip.address()}:4000/api/`
+    baseURL:`localhost:4000/api/`
 })
 
 export default apiClient;
